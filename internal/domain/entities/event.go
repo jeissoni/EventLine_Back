73cb396db07cfae2ae6b70134d7539fd0576ba93
jeissoni <jeissoni_22@hotmail.com>
2025-03-18 21:10:11 -0500

    Refactor Event struct and service methods to use string for event_id

--- internal/domain/entities/event.go
+++ internal/domain/entities/event.go
@@ -10,13 +10,11 @@ type Event struct {
 	EventID             uuid.UUID  `json:"event_id" db:"event_id"`
 	OrganizerID         uuid.UUID  `json:"organizer_id" db:"organizer_id"`
 	CategoryID          *uuid.UUID `json:"category_id,omitempty" db:"category_id"` // Puntero para permitir valores NULL
-	VenueID             *uuid.UUID `json:"venue_id,omitempty" db:"venue_id"`       // Puntero para permitir valores NULL
 	Title               string     `json:"title" db:"title"`
 	Description         string     `json:"description" db:"description"`
 	ShortDescription    string     `json:"short_description" db:"short_description"`
 	StartDate           time.Time  `json:"start_date" db:"start_date"`
 	EndDate             time.Time  `json:"end_date" db:"end_date"`
-	DoorsOpenDate       *time.Time `json:"doors_open_date,omitempty" db:"doors_open_date"` // Puntero para permitir valores NULL
 	IsPublished         bool       `json:"is_published" db:"is_published"`
 	IsFeatured          bool       `json:"is_featured" db:"is_featured"`
 	IsPrivate           bool       `json:"is_private" db:"is_private"`
--- internal/ports/events.go
+++ internal/ports/events.go
@@ -4,14 +4,14 @@ import domain "github.com/jeissoni/EventLine/internal/domain/entities"
 
 type EventService interface {
 	Create(events domain.Event) error
-	GetByID(id int) (domain.Event, error)
+	GetByID(event_id string) (domain.Event, error)
 	GetAll() ([]domain.Event, error)
 	Delete(id int) error
 }
 
 type EventRepository interface {
 	Guardar(events domain.Event) error
-	GetByID(id int) (domain.Event, error)
+	GetByID(event_id string) (domain.Event, error)
 	GetAll() ([]domain.Event, error)
 	Delete(id int) error
 }
