23179584458aa21bae06565046d0c056b109f850
jeissoni <jeissoni_22@hotmail.com>
2025-03-22 21:27:40 -0500

    Update Delete handler to accept UUID for event_id and improve error handling

--- cmd/api/handlers/events/delete.go
+++ cmd/api/handlers/events/delete.go
@@ -2,19 +2,22 @@ package events
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 func (h EventHandler) Delete(c *fiber.Ctx) error {
 
-	id, err := c.ParamsInt("id")
+	id_event := c.Params("id")
+
+	_, err := uuid.Parse(id_event)
 
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": err.Error(),
+			"error": "Invalid UUID",
 		})
 	}
 
-	err = h.EventService.Delete(id)
+	err = h.EventService.Delete(id_event)
 
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
