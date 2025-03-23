5c5b1231482ad78ca2cbf2b47672a9b6110437c8
jeissoni <jeissoni_22@hotmail.com>
2025-03-22 21:27:22 -0500

    Refactor Delete method in Postgres event repository to use string for event_id

--- internal/repositories/postgres/events/delete.go
+++ internal/repositories/postgres/events/delete.go
@@ -1,7 +1,7 @@
 package events
 
-func (r Repository) Delete(id int) error {
-	_, err := r.Database.Exec("DELETE FROM events WHERE id = $1", id)
+func (r Repository) Delete(event_id string) error {
+	_, err := r.Database.Exec("DELETE FROM events WHERE event_id = $1", event_id)
 
 	if err != nil {
 		return err
